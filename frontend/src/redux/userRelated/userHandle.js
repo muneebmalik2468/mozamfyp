eb2361fb1cded5e548ecd28c6800f8aeb5f3f2a5
muneebcfrontiers <muneeb.khalid@cfrontiers.com>
2025-07-04 03:06:34 +0500

    frontend commit

--- frontend/src/redux/userRelated/userHandle.js
+++ frontend/src/redux/userRelated/userHandle.js
@@ -2,8 +2,12 @@
 
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost:5000",
+// });
+
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.REACT_APP_BASE_URL || "http://localhost:5000",
 });
 
 api.interceptors.request.use(

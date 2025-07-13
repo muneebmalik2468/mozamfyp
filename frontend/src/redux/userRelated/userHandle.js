c99acf6b723604b9971b0284258ae3229fff07f1
muneebcfrontiers <muneeb.khalid@cfrontiers.com>
2025-07-14 01:47:23 +0500

    update base url in frontend

--- frontend/src/redux/userRelated/userHandle.js
+++ frontend/src/redux/userRelated/userHandle.js
@@ -7,7 +7,7 @@ import axios from "axios";
 // });
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000",
+  baseURL: process.env.REACT_APP_BASE_URL || "https://mozamfyp-backend.vercel.app",
 });
 
 api.interceptors.request.use(
